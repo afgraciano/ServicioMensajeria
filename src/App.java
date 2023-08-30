89a56e0b9a047b4364694241d1a7b159d41ffc23
Andres Felipe Graciano Monsalve <72512285+afgraciano@users.noreply.github.com>
2023-08-30 09:02:53 -0500

    3 comit Patron Proxy o apoderado(agentesmitmatrix)

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-/*Patron Proxy o estructural para proteccion y ejecucion remota*/
+/*Patron Proxy o apoderado o agente que  para proteccion y ejecucion remota que controla el acceso al objeto original*/
 
 public class App {
     public static void main(String[] args) throws Exception {
